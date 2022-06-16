2639aa5dfb7c36da69daf2d2a28d58da482e02ce
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-06-16 12:12:18 -0400

    Try navigating with router & navController

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private navController: NavController,
+    private router: Router
+  ) {}
+
+  navigateToPageAsComponent(){
+    // These all work.
+    // this.navController.navigateForward('/tabs/tab1/page-as-component');
+    this.navController.navigateForward(['tabs', 'tab1', 'page-as-component']);
+    // this.router.navigate(['tabs', 'tab1', 'page-as-component']);
+  }
 
 }
